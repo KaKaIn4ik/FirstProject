fdc1b977382e40c3efeb17f92f1486e6a9dbf037
EduardKungurtsev <KaKaIn4ik@inbox.ru>
2022-03-01 23:02:16 +0700

    Initial Commit

---
+++ FirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Кунгурцев Эдуард Сергеевич on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
