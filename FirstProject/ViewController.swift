8a49fefa7e90cd45c83cba216dd9a72e18a32c6a
EduardKungurtsev <e.kungurtsev@2gis.ru>
2022-03-03 20:54:52 +0700

    Update

--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        colorSwitchingButton.layer.cornerRadius = 10
 
     }
 
